36d658ba0b0d24ca34c082c19a20452a80f2da80
Roseanne Feng <roseanne.feng@gmail.com>
2023-07-07 18:05:29 -0400

    Refactor logic for shortening long partition keys

--- dpk.js
+++ dpk.js
@@ -5,27 +5,19 @@ exports.deterministicPartitionKey = (event) => {
   const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
 
-  function shortenCandidate(candid) {
-    return crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-
   if (!(event)) {
     candidate = TRIVIAL_PARTITION_KEY;
   } else if (typeof event.partitionKey == "string") {
     candidate = event.partitionKey;
-    if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-      candidate = shortenCandidate(candidate);
-    }
   } else if (event.partitionKey) {
     candidate = JSON.stringify(event.partitionKey);
-    if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-      candidate = shortenCandidate(candidate);
-    }
   } else {
     const data = JSON.stringify(event);
     candidate = crypto.createHash("sha3-512").update(data).digest("hex");
   }
 
-
+  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
+    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+  }
   return candidate;
 };
